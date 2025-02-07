1b84975b8ab524547eae355e1d3cb8dcc3f5ca27
Ander Pierce <Anderpierce@protonmail.com>
2025-02-06 16:31:29 -0800

    Did some work on chapter 3

---
+++ Northwind.Console.EFCore/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.Models;
+
+[Index("CategoryName", Name = "CategoryName")]
+
+// Category is partial so that you can add your own Category class, and if you re-run the scaffolding tool it doesn't overwrite ur class
+public partial class Category //Humanizer third-party package automatically replaces the Categories table name with Category. Neat!
+{
+    [Key] // Explicitly indicate CategoryId is key
+    public int CategoryId { get; set; }
+
+    [StringLength(15)]
+    public string CategoryName { get; set; } = null!;
+
+    [Column(TypeName = "ntext")]
+    public string? Description { get; set; }
+
+    [Column(TypeName = "image")]
+    public byte[]? Picture { get; set; }
+
+    [InverseProperty("Category")] // Identifies the foreign key on the Product entity class
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ Northwind.Console.EFCore/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore; // To use QueryString
+using Microsoft.Data.SqlClient; // To use SqlConnectonsStringBuilder
+using Northwind.Models; // To use NorthwindDb and the rest
+
+SqlConnectionStringBuilder builder = new();
+
+builder.DataSource = ".";
+builder.IntegratedSecurity = true;
+
+DbContextOptionsBuilder<NorthwindDb> options = new();
+options.UseSqlServer(builder.ConnectionString);
+
+using (NorthwindDb db = new(options.Options)){
+    Write("Enter a unit price:");
+    string? priceText = ReadLine();
+
+    if(!decimal.TryParse(priceText, out decimal price)){
+        WriteLine("You must enter a valid unit price");
+        return;
+    }
+
+// We have to use a var because we are projecting into an anonymous type
+var products = db.Products
+    .Where(p=>p.UnitPrice > price)
+    .Select(p => new {p.ProductId, p.ProductName, p.UnitPrice});
+
+#region Products table from query
+WriteLine("-", 60);
+WriteLine("| {0,5} | {1,-35} | {2,8} |", "Id", "Name", "Price");
+WriteLine("-", 60);
+
+foreach(var p in products){
+    WriteLine("| {0,5} | {1,-35} | {2,8:C} |", p.ProductId, p.ProductName, p.UnitPrice);
+}
+
+#endregion
+
+
+
+
+    WriteLine("-",60);
+    WriteLine(products.ToQueryString());
+    WriteLine();
+    WriteLine($"Provider: {db.Database.ProviderName}");
+    WriteLine($"Connection: {db.Database.GetConnectionString()}");
+}
